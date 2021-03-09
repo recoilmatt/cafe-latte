b7ae76189be28be7237ebca2361fc47f5dbc09c8
recoilmatt <recoilmatt@gmail.com>
2021-03-08 21:44:53 -0800

    22

--- src/App2.js
+++ src/App2.js
@@ -13,7 +13,12 @@ import Details from "./pages/Details";
 import Cart from "./pages/Cart";
 
 //React Router
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 
 /* //Auth Pages
 import Signup from "./auth/Signup";
@@ -34,6 +39,23 @@ import { Scrollbars } from "react-custom-scrollbars";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
+//Css Transition
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+
+const AnimatedSwitch = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition key={location.key} classNames="fade" timeout={0}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/products/:id" component={Details} />
+        <Route path="/cart" component={Cart} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/locations" component={LocationsSection} />
+      </Switch>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
 function App2() {
   return (
     <DataProvider>
@@ -45,14 +67,7 @@ function App2() {
           style={{ width: "100vw", height: "100vh" }}
         >
           <HeaderTest />
-          <Switch>
-            <Route exact path="/" component={Home} />
-
-            <Route path="/products/:id" component={Details} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/menu" component={Menu} />
-            <Route path="/locations" component={LocationsSection} />
-          </Switch>
+          <AnimatedSwitch />
 
           <FooterTest />
         </Scrollbars>
