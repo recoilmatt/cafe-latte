4b57cb55f8a3dce344491745bd976683bce03624
recoilmatt <recoilmatt@gmail.com>
2021-03-08 18:06:35 -0800

    12334

--- src/App2.js
+++ src/App2.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //Components
 import Home from "./pages/Home";
-import Header from "./components/Header";
+
 /* import Footer from "./Footer"; */
 import FooterTest from "./components/FooterTest";
 
@@ -11,12 +11,7 @@ import Menu from "./pages/Menu";
 import LocationsSection from "./pages/LocationsSection";
 
 //React Router
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  withRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //DataProvider
 import DataProvider from "./components/DataProvider";
@@ -24,40 +19,27 @@ import DataProvider from "./components/DataProvider";
 //Scrollbars
 import { Scrollbars } from "react-custom-scrollbars";
 
-//Css Transition
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-
 //Css Files
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
-const AnimatedSwitch = withRouter(({ location }) => (
-  <TransitionGroup>
-    <CSSTransition key={location.key} classNames="fade" timeout={0}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-
-        <Route path="/menu" component={Menu} />
-        <Route path="/locations" component={LocationsSection} />
-      </Switch>
-    </CSSTransition>
-  </TransitionGroup>
-));
 
 function App() {
   return (
     <DataProvider>
       <Router>
-        <Scrollbars
-          autoHide
-          autoHideTimeout={1000}
-          autoHideDuration={200}
-          style={{ width: "100vw", height: "100vh" }}
-        >
-          <Header />
-          <AnimatedSwitch />
-          <FooterTest />
-        </Scrollbars>
+        <Switch>
+          <Scrollbars
+            autoHide
+            autoHideTimeout={1000}
+            autoHideDuration={200}
+            style={{ width: "100vw", height: "100vh" }}
+          >
+            <Route exact path="/" component={Home} />
+            <Route path="/menu" component={Menu} />
+            <Route path="/locations" component={LocationsSection} />
+            <FooterTest />
+          </Scrollbars>
+        </Switch>
       </Router>
     </DataProvider>
   );
