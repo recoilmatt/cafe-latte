34003833f9b8b39b281fe57c470b4de14b84c070
recoilmatt <recoilmatt@gmail.com>
2021-03-08 18:31:21 -0800

    1111

--- src/App2.js
+++ src/App2.js
@@ -2,7 +2,7 @@ import React from "react";
 
 //Components
 import Home from "./pages/Home";
-import Header from "./components/Header";
+import HeaderTest from "./components/HeaderTest";
 /* import Footer from "./Footer"; */
 import FooterTest from "./components/FooterTest";
 
@@ -11,9 +11,6 @@ import Menu from "./pages/Menu";
 import LocationsSection from "./pages/LocationsSection";
 import Details from "./pages/Details";
 import Cart from "./pages/Cart";
-
-//Context
-import { AuthProvider } from "./contexts/AuthContext";
 
 //React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -39,32 +36,30 @@ import "./App.css";
 function App2() {
   return (
     <DataProvider>
-      <AuthProvider>
-        <Router>
-          <Scrollbars
-            autoHide
-            autoHideTimeout={1000}
-            autoHideDuration={200}
-            style={{ width: "100vw", height: "100vh" }}
-          >
-            <Header />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute path="/update-profile" component={UpdateProfile} />
-              <Route path="/signup" component={Signup} />
-              <Route path="/login" component={Login} />
-              <Route path="/forgot-password" component={ForgotPassword} />
-              <Route path="/products/:id" component={Details} />
-              <Route path="/cart" component={Cart} />
-              <Route path="/menu" component={Menu} />
-              <Route path="/locations" component={LocationsSection} />
-            </Switch>
+      <Router>
+        <Scrollbars
+          autoHide
+          autoHideTimeout={1000}
+          autoHideDuration={200}
+          style={{ width: "100vw", height: "100vh" }}
+        >
+          <HeaderTest />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/login" component={Login} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+            <Route path="/products/:id" component={Details} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/menu" component={Menu} />
+            <Route path="/locations" component={LocationsSection} />
+          </Switch>
 
-            <FooterTest />
-          </Scrollbars>
-        </Router>
-      </AuthProvider>
+          <FooterTest />
+        </Scrollbars>
+      </Router>
     </DataProvider>
   );
 }
--- src/components/HeaderTest.js
+++ src/components/HeaderTest.js
@@ -1,13 +1,90 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useContext } from "react";
+import "./Header.css";
 
-function HeaderTest() {
+import { Link } from "react-router-dom";
+import { DataContext } from "./DataProvider";
+
+import Menu from "./svg/bars-solid.svg";
+import Close from "./svg/times-solid.svg";
+
+import styled from "styled-components";
+
+const HeaderStyle = styled.header`
+  width: 100%;
+  min-height: 100px;
+  overflow: hidden;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  border-bottom: 1px solid #ccc;
+`;
+
+const Links = styled.ul`
+  @media (max-width: 700px) {
+    position: fixed;
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 100vh;
+    background: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    opacity: 0.98;
+    z-index: 99;
+    transition: 0.5s ease-in;
+    padding-left: 0;
+  }
+`;
+
+export default function Header() {
+  const [menu, setMenu] = useState(false);
+  const value = useContext(DataContext);
+  const [cart] = value.cart;
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
+  const styleMenu = {
+    left: menu ? 0 : "-100%",
+  };
+
   return (
-    <div>
-      <Link to="/menu">Menu</Link>
-      <Link to="/locations">Locations</Link>
-    </div>
+    <HeaderStyle>
+      <div className="menu" onClick={toggleMenu}>
+        <img src={Menu} alt="" width="30" />
+      </div>
+      <Links style={styleMenu}>
+        <li>
+          <Link to="/" onClick={toggleMenu}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/menu" onClick={toggleMenu}>
+            Menu
+          </Link>
+        </li>
+        <li>
+          <Link to="/Locations" onClick={toggleMenu}>
+            Locations
+          </Link>
+        </li>
+
+        <li>
+          <Link to="/cart">Cart ({cart.length})</Link>
+        </li>
+        <li
+          onClick={() => {
+            toggleMenu();
+          }}
+        >
+          <img src={Close} alt="" width="30" className="menu" />
+        </li>
+      </Links>
+    </HeaderStyle>
   );
 }
-
-export default HeaderTest;
