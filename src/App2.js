5f5ed34a132cd1d100b1319be0381a3e1d678eb0
recoilmatt <recoilmatt@gmail.com>
2021-03-08 17:45:47 -0800

    111

--- src/App2.js
+++ src/App2.js
@@ -1,21 +1,13 @@
 import React from "react";
 
 //Components
-import Home from "./pages/Home";
-/* import Header from "./components/Header"; */
-/* import Footer from "./Footer"; */
 import FooterTest from "./components/FooterTest";
 
 //Pages
 import Menu from "./pages/Menu";
-import LocationsSection from "./pages/LocationsSection";
-import Details from "./pages/Details";
 
 //React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-//Scrollbars
-import { Scrollbars } from "react-custom-scrollbars";
 
 //Css Transition
 
@@ -26,23 +18,11 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <Scrollbars
-        autoHide
-        autoHideTimeout={1000}
-        autoHideDuration={200}
-        style={{ width: "100vw", height: "100vh" }}
-      >
-        <Switch>
-          <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/" component={Menu} />
+      </Switch>
 
-          <Route path="/products/:id" component={Details} />
-
-          <Route path="/menu" component={Menu} />
-          <Route path="/locations" component={LocationsSection} />
-        </Switch>
-
-        <FooterTest />
-      </Scrollbars>
+      <FooterTest />
     </Router>
   );
 }
