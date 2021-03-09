cf8391c9717ea7357e0b3c16c2747593a7b6ccbb
recoilmatt <recoilmatt@gmail.com>
2021-03-08 17:36:07 -0800

    12211

--- src/App2.js
+++ src/App2.js
@@ -10,27 +10,14 @@ import FooterTest from "./components/FooterTest";
 import Menu from "./pages/Menu";
 import LocationsSection from "./pages/LocationsSection";
 import Details from "./pages/Details";
-import Cart from "./pages/Cart";
 
 //React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-//Auth Pages
-import Signup from "./auth/Signup";
-import Dashboard from "./auth/Dashboard";
-import Login from "./auth/Login";
-import PrivateRoute from "./auth/PrivateRoute";
-import ForgotPassword from "./auth/ForgotPassword";
-import UpdateProfile from "./auth/UpdateProfile";
-
-//DataProvider
-import DataProvider from "./components/DataProvider";
 
 //Scrollbars
 import { Scrollbars } from "react-custom-scrollbars";
 
 //Css Transition
-import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 //Css Files
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -38,27 +25,25 @@ import "./App.css";
 
 function App() {
   return (
-    <DataProvider>
-      <Router>
-        <Scrollbars
-          autoHide
-          autoHideTimeout={1000}
-          autoHideDuration={200}
-          style={{ width: "100vw", height: "100vh" }}
-        >
-          <Switch>
-            <Route exact path="/" component={Home} />
+    <Router>
+      <Scrollbars
+        autoHide
+        autoHideTimeout={1000}
+        autoHideDuration={200}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Switch>
+          <Route path="/" component={Home} />
 
-            <Route path="/products/:id" component={Details} />
+          <Route path="/products/:id" component={Details} />
 
-            <Route path="/menu" component={Menu} />
-            <Route path="/locations" component={LocationsSection} />
-          </Switch>
+          <Route path="/menu" component={Menu} />
+          <Route path="/locations" component={LocationsSection} />
+        </Switch>
 
-          <FooterTest />
-        </Scrollbars>
-      </Router>
-    </DataProvider>
+        <FooterTest />
+      </Scrollbars>
+    </Router>
   );
 }
 
