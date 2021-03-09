55deb5b71a1dc2b94499397b0dcf20d394349125
recoilmatt <recoilmatt@gmail.com>
2021-03-08 16:39:30 -0800

    test3

--- src/App2.js
+++ src/App2.js
@@ -3,23 +3,10 @@ import React from "react";
 //Components
 import Home from "./pages/Home";
 import Header from "./components/Header";
-/* import Footer from "./Footer"; */
 import FooterTest from "./components/FooterTest";
-
-//Pages
-import Menu from "./pages/Menu";
-import LocationsSection from "./pages/LocationsSection";
-import Details from "./pages/Details";
-import Cart from "./pages/Cart";
-
-//Context
-import { AuthProvider } from "./contexts/AuthContext";
 
 //React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-//DataProvider
-import DataProvider from "./components/DataProvider";
 
 //Css Files
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -27,23 +14,14 @@ import "./App.css";
 
 function App() {
   return (
-    <DataProvider>
-      <AuthProvider>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
 
-            <Route path="/products/:id" component={Details} />
-            <Route path="/cart" component={Cart} />
-            <Route path="/menu" component={Menu} />
-            <Route path="/locations" component={LocationsSection} />
-          </Switch>
-
-          <FooterTest />
-        </Router>
-      </AuthProvider>
-    </DataProvider>
+      <FooterTest />
+    </Router>
   );
 }
 
